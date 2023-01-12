30676e35b8703772b64f7c14c70a13be2804f804
MistySunMew <Michael.e.forbes@comcast.com>
2023-01-12 11:00:32 -0800

    Add project files.

---
+++ ModelBidingHW/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBidingHW.Models;
+using System.Diagnostics;
+
+namespace ModelBidingHW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Create(VideoGame g)
+        {
+            //Validate
+
+            //Add to Database
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ModelBidingHW/Models/VideoGame.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBidingHW.Models;
+/// <summary>
+/// Object to hold a single video game
+/// </summary>
+public class VideoGame
+{
+    /// <summary>
+    /// Title/name of the video game
+    /// </summary>
+    public required string Title { get; set; }
+
+    /// <summary>
+    /// MSRP/Current market price of the video game
+    /// </summary>
+    [Range(0, int.MaxValue)]
+    public required double Price { get; set; }
+
+    /// <summary>
+    /// ESRB or PEGI age rating of the video game
+    /// </summary>
+    public required string AgeRating { get; set; }
+
+    /// <summary>
+    /// Publisher for the video game if NULL than is the same as Developer
+    /// </summary>
+    public string? Publisher { get; set; }  
+
+    /// <summary>
+    /// Developer of the video game
+    /// </summary>
+    public required string Developer { get; set; }
+}
+
